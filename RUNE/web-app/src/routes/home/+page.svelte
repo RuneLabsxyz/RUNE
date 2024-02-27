262fecb2e18b7ed8cef4dba9b50fc6208d0fbf98
0xmugen <pierre.semanne@gmail.com>
2024-02-27 21:48:27 +0100

    add: shad cn

--- RUNE/web-app/src/routes/home/+page.svelte
+++ RUNE/web-app/src/routes/home/+page.svelte
@@ -1,6 +1,38 @@
 <script>
-
+    const logo = 'images/logo.png';
 </script>
 
+<div class="w-full h-[4rem] bg-slate-600 text-white flex justify-between items-center">
+    <div class="flex items-center">
+        <img src={logo} alt="Company logo" class="h-[4rem] p-2">
+    </div>  
+    <div class="flex text-center items-center">
+        <div class="p-3">Section 1</div>
+        <div class="p-3">Section 2</div>
+    </div>
+    <div class="">
+        qsdfqsdfqsdfqsdfqsfdqsdfqsdfqsdfqdsfqsdf
+    </div>
+</div>
 
-test !
+<div class="w-full flex justify-between text-white">
+    <div class="w-1/4 bg-slate-700">
+        <div class="flex justify-center p-5 text-2xl uppercase">
+            Library
+        </div>
+        <div>
+            <div class="p-5">Section 1</div>
+            <div class="p-5">Section 2</div>
+            <div class="p-5">Section 3</div>
+            <div class="p-5">Section 4</div>
+        </div>
+    </div>
+    <div class="w-3/4">mlskqdf</div>
+</div>
+
+
+<style>
+    :root {
+        background-color: #1f2937;
+    }
+</style>
