51d5f659a56a12ec7b0369b583359a7066fff8bf
0xmugen <pierre.semanne@gmail.com>
2024-02-27 23:32:19 +0100

    rebase

---
+++ Electron/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
+    let isLoggedIn = false;
+
+    onMount(() => {
+        if (isLoggedIn) {
+            goto('/home');
+        } else {
+            goto('/home');
+        }
+    });
+
+    
+</script>
+aaa
---
+++ Electron/svelte.config.js
@@ -0,0 +1,10 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: preprocess()
+};
+export default config;
