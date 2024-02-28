453da3e709c8121e4e034f9daff418e549d237c3
0xmugen <pierre.semanne@gmail.com>
2024-02-28 16:27:28 +0100

    routing for game list

--- svelte-app/src/lib/CustomCard.svelte
+++ svelte-app/src/lib/CustomCard.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import * as Card from "$lib/components/ui/card";
     import { Button } from "$lib/components/ui/button";
+    import { goto } from "$app/navigation";
 
     import type { Game } from "$lib/types";
 
@@ -10,17 +11,18 @@
         console.log('Added to Library', cardData.id);
     }
 </script>
-
-<Card.Root>
-    <Card.Header>
-      <Card.Title>{cardData.name}</Card.Title>
-      <Card.Description>{cardData.description}</Card.Description>
-    </Card.Header>
-    <Card.Content>
-      <p>Image</p>
-      <p>{cardData.chain}</p>
-    </Card.Content>
-    <Card.Footer>
-        <Button on:click={addToLibrary}>Add to Library</Button>
-    </Card.Footer>
-</Card.Root>
+    <button on:click={() => goto(`/game/${cardData.id}/`)}>
+    <Card.Root>
+        <Card.Header>
+        <Card.Title>{cardData.name}</Card.Title>
+        <Card.Description>{cardData.description}</Card.Description>
+        </Card.Header>
+        <Card.Content>
+        <p>Image</p>
+        <p>{cardData.chain}</p>
+        </Card.Content>
+        <Card.Footer>
+            <Button on:click={addToLibrary}>Add to Library</Button>
+        </Card.Footer>
+    </Card.Root>
+</button>
