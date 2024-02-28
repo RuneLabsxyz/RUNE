be4f94b9d8cc812851e53bbde3193bd4a060ee70
0xmugen <pierre.semanne@gmail.com>
2024-02-28 17:21:03 +0100

    back feat

--- svelte-app/src/routes/game/[slug]/+page.svelte
+++ svelte-app/src/routes/game/[slug]/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import type { Game } from "$lib/types";
+  import { Button } from "$lib/components/ui/button";
+  import { goto } from '$app/navigation';
+
+  const back = '/svgs/back.svg'
 
 
   export let data;
   let gameData: Game;
 
-async function load(data) {
+async function load(data: any) {
     const res = await fetch(`http://localhost:3000/api/games/${data.slug}`);
     const games = await res.json();
     return { games };
@@ -22,5 +26,38 @@ onMount(async () => {
 });
 
 </script>
+{#if gameData}
 
-<h1>{gameData}</h1>
+<div class="flex flex-col h-screen bg-slate-700">
+
+  <!-- Content -->
+  <div class="w-full flex-1 flex justify-between text-white">
+      <div class="w-1/4 bg-slate-700">
+          <div class="flex pt-10 pb-4 px-20 text-2xl uppercase items-center">
+              <Button variant="link" size="icon" on:click={() => goto(`/home/explore`)}>
+                <img src={back} alt="Company logo" class="h-[30px] px-3 mr-10">
+              </Button>
+              {gameData.name}
+          </div>
+          <div class="px-20 text-black">
+          </div>
+          <div>
+
+          </div>
+      </div>
+      <div class="w-3/4 bg-slate-700 ">
+          <div class="flex justify-between item-center border-b py-3 mr-10">
+              <div></div>
+              <div class="flex item-center ">
+              </div>
+          </div>
+          <div>
+              <div class="flex flex-wrap justify-between px-10 py-10">
+
+              </div>
+          </div>
+      </div>
+  </div>
+
+</div>
+{/if}
