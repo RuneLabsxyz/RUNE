be68b3ffc8e9dcf3324e912b57ae8f96953fb434
0xmugen <pierre.semanne@gmail.com>
2024-02-28 14:15:02 +0100

    feat: refactore pages

---
+++ svelte-app/src/routes/home/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+    const logo = 'images/logo.png';
+</script>
+
+<!-- Header -->
+    <div class="w-full h-[4rem] bg-slate-600 text-white flex justify-between items-center">
+        <div class="flex items-center">
+            <img src={logo} alt="Company logo" class="h-[4rem] p-2">
+        </div>  
+        <div class="flex text-center items-center">
+            <div class="p-3">Section 1</div>
+            <div class="p-3">Section 2</div>
+        </div>
+        <div class="">
+            qsdfqsdfqsdfqsdfqsfdqsdfqsdfqsdfqdsfqsdf
+        </div>
+    </div>
+
+
+<slot />
