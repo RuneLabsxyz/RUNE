20c94e268d9abf83e067437549d6110ac24cbad3
Veikkan <pierre.semanne@gmail.com>
2024-03-22 02:57:01 +0100

    feat: simple metamask support

--- src/lib/overlay/metamask/Metamask.svelte
+++ src/lib/overlay/metamask/Metamask.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     let hasMetaMask = false;
-    let accounts = [];
+    let connectedToMetaMask = false;
+    let accounts: string[] = [];
+    
 
 
     if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
@@ -12,19 +15,32 @@
         try {
             const _accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
             accounts = _accounts;
+            connectedToMetaMask = true;
         } catch (error) {
             console.error('Error connecting to MetaMask', error);
         }
         }
     }
-        
+
+    onMount(async () => {
+      if (hasMetaMask) {
+        await connectMetaMask();
+      }
+    });
+
 </script>
 
+<div class="text-white">
 {#if hasMetaMask}
-  <button on:click={connectMetaMask}>Connect MetaMask</button>
-  {#if accounts.length > 0}
-    <p>Connected account: {accounts[0]}</p>
+  {#if connectedToMetaMask}
+    {#if accounts.length > 0}
+      <p>Connected account: {accounts[0]}</p>
+    {/if}
+    {:else}
+    <button on:click={connectMetaMask}>Connect MetaMask</button>
   {/if}
+
 {:else}
   <p>Please install MetaMask.</p>
 {/if}
+</div>
